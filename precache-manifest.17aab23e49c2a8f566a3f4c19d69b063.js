e743d16073b2c4fabd1f114901f5ac4c8d385d37
Bernat <bernatesquirol@gmail.com>
2020-04-06 21:56:20 +0200

    v0.01

---
+++ precache-manifest.17aab23e49c2a8f566a3f4c19d69b063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cda9cf863352f7d62e9e2f0de7fa3c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c163afca3a4cb9a5f4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5adbe7b1ccf3dbc98f7",
+    "url": "/static/js/2.63333260.chunk.js"
+  },
+  {
+    "revision": "eda12d3c1c5963357f19209e410c0b51",
+    "url": "/static/js/2.63333260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5c163afca3a4cb9a5f4",
+    "url": "/static/js/main.f0fe141d.chunk.js"
+  },
+  {
+    "revision": "c48467e20791b2d08039",
+    "url": "/static/js/runtime-main.092d0c88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
